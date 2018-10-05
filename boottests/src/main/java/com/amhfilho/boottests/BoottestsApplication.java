e2b0d6c94cac33620d3df64c285b55aea6fff9d8
Antonio Mario Henriques Filho <amhfilho@gmail.com>
2018-10-04 21:39:28 -0300

    Project bootstrap

---
+++ boottests/src/main/java/com/amhfilho/boottests/BoottestsApplication.java
@@ -0,0 +1,12 @@
+package com.amhfilho.boottests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoottestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoottestsApplication.class, args);
+	}
+}
